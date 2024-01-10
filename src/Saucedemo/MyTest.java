078ee3f7cbe7128dad6a971405f8a2f8b3cc5c65
noor yousef <noor.non1993@gmail.com>
2024-01-10 23:35:53 +0300

    'z'

--- src/Saucedemo/MyTest.java
+++ src/Saucedemo/MyTest.java
@@ -10,108 +10,104 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class MyTest extends Parameter {
-	Parameter Swag =new Parameter();
-	
-@BeforeTest
-public void SetUp() {
-	
-	driver.get(Url);
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(3));
+	Parameter Swag = new Parameter();
 
-	WebElement User=driver.findElement(By.id("user-name"));
-	User.sendKeys(Swag.theUsername);
-	
-	WebElement Password=driver.findElement(By.id("password"));
-	Password.sendKeys(Swag.Password);
-	
-	WebElement LogIn=driver.findElement(By.className("submit-button"));
-    LogIn.click();
+	@BeforeTest
+	public void SetUp() {
 
-}
+		driver.get(Url);
+		driver.manage().window().maximize();
 
+		WebElement User = driver.findElement(By.id("user-name"));
+		User.sendKeys(Swag.theUsername);
 
+		WebElement Password = driver.findElement(By.id("password"));
+		Password.sendKeys(Swag.Password);
 
-@Test(priority=1)
-public void FilterItems() throws InterruptedException {
-	Thread.sleep(1000);
+		WebElement LogIn = driver.findElement(By.className("submit-button"));
+		LogIn.click();
+		driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(5));
 
-	WebElement Sort=driver.findElement(By.cssSelector(".product_sort_container"));
-	Sort.click();
-	Thread.sleep(1000);
-
-	List<WebElement> Filter=Sort.findElements(By.tagName("option"));
-	Filter.get(3).click();
-	
-	Thread.sleep(1000);
-
-}
-
-@Test(priority=2)
-public void AddToCart() {
-	
-	List<WebElement> Items=driver.findElements(By.className("inventory_list"));
-	for(int i=0 ; i < Items.size() ; i++) {
 		
-		if(i==0) {
-			
-		WebElement Choose=driver.findElement(By.id("add-to-cart-sauce-labs-fleece-jacket"));
-		Choose.click();
+
 		
-			
-			
+
+	}
+
+	@Test(priority = 1)
+	public void FilterItems() throws InterruptedException {
+
+		WebElement Sort = driver.findElement(By.cssSelector(".product_sort_container"));
+		Sort.click();
+		Thread.sleep(1000);
+
+		List<WebElement> Filter = Sort.findElements(By.tagName("option"));
+		Filter.get(3).click();
+
+		Thread.sleep(10000);
+
+	}
+
+	@Test(priority = 2)
+	public void AddToCart() throws InterruptedException {
+		WebElement myList= driver.findElement(By.className("inventory_list"));
+		List<WebElement> AllItem=myList.findElements(By.tagName("div"));
+
+                       AllItem.get(0).findElement(By.className("btn_primary")).click();
+					Thread.sleep(5000);
+
+                    AllItem.get(5).findElement(By.className("btn_primary")).click();
+
+					driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+
+				
+
 		}
-		
-		 if(i == Items.size()-1) {
-			
-			WebElement Choosetwo=driver.findElement(By.id("add-to-cart-sauce-labs-onesie"));
-			Choosetwo.click();
-			
-		}
-		
-	} 
-	
-	
-}
 
+		@Test(priority = 3)
+		public void CheckOut() throws InterruptedException {
+			driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+			Thread.sleep(5000);
 
-@Test(priority=3)
-public void CheckOut() {
-	
-	WebElement Cart=driver.findElement(By.id("shopping_cart_container"));
-	Cart.click();
-	
-	WebElement Checkout=driver.findElement(By.id("checkout"));
-	Checkout.click();
+			WebElement Cart = driver.findElement(By.id("shopping_cart_container"));
+			Cart.click();
 
-	WebElement Firstname = driver.findElement(By.id("first-name"));
-	WebElement Lastname = driver.findElement(By.xpath("//input[@id='last-name']"));
-	WebElement Post = driver.findElement(By.id("postal-code"));
+			WebElement Checkout = driver.findElement(By.id("checkout"));
+			Checkout.click();
 
-	Firstname.sendKeys(Swag.fname);
-	Lastname.sendKeys(Swag.lname);
-	Post.sendKeys(Swag.postal);
+			WebElement Firstname = driver.findElement(By.id("first-name"));
+			WebElement Lastname = driver.findElement(By.xpath("//input[@id='last-name']"));
+			WebElement Post = driver.findElement(By.id("postal-code"));
 
-	WebElement Continue = driver.findElement(By.id("continue"));
-	Continue.click();
+			Firstname.sendKeys(Swag.fname);
 
-	WebElement finish = driver.findElement(By.id("finish"));
-	finish.click();
+			Lastname.sendKeys(Swag.lname);
+
+			Post.sendKeys(Swag.postal);
+
+			WebElement Continue = driver.findElement(By.id("continue"));
+			Continue.click();
+
+			driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(3));
+
+			WebElement finish = driver.findElement(By.id("finish"));
+			finish.click();
+			driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(3));
 
 // Assertion for Checkout
-	WebElement complete = driver.findElement(By.className("complete-header"));
-	String actual = complete.getText();
+			WebElement complete = driver.findElement(By.className("complete-header"));
+			String actual = complete.getText();
 
-	Assert.assertEquals(actual.contains("Thank"), true);
-	
-	WebElement Ok=driver.findElement(By.className("pony_express"));
-	Assert.assertEquals(Ok.isDisplayed(), true);
+			Assert.assertEquals(actual.contains("Thank"), true);
 
-}
+			WebElement Ok = driver.findElement(By.className("pony_express"));
+			Assert.assertEquals(Ok.isDisplayed(), true);
 
-@AfterTest
-public void Ending() {
-	
-}
+		}
 
-}
+		@AfterTest
+		public void Ending() {
+
+		}
+
+	}
